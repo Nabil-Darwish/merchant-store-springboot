7d1174699d76f79fbda1086c94d2048b4f1840b2
Nabil Darwish <s2019904@ed.ac.uk>
2025-10-30 23:07:28 +0800

    initial commit

---
+++ src/main/java/application/merchantStore/MerchantStoreApplication.java
@@ -0,0 +1,13 @@
+package application.merchantStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchantStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerchantStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/application/merchantStore/MerchantStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package application.merchantStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchantStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
